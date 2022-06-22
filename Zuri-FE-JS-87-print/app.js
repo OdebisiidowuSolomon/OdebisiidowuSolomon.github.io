a4f9864fdeb6124086254c7c5c8ec88a4466dcb1
Solomon <odebisiidowu09@gmail.com>
2022-06-22 14:06:12 +0100

    added changes

--- Zuri-FE-JS-87-print/app.js
+++ Zuri-FE-JS-87-print/app.js
@@ -1,4 +1,11 @@
-document.write(`Name: Odebisi Solomon Idowu\n`);
-document.write('My Height is: 5.8ft\n');
-document.write('Country: Nigeria');
-console.log('first')
+const _name = "Name: Odebisi Solomon Idowu";
+const height = "My Height is: 5.8ft";
+const country = 'Country: Nigeria';
+
+document.write(_name);
+document.write(height);
+document.write(country);
+
+alert(_name+ '\n' + height +'\n' + country)
+
+console.log(_name+ '\n' + height + '\n' + country)
