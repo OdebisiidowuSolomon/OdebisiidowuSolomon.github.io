54d0179fc16ab2d86208eaef8b6ea4cf884b9556
Olamilekan <odebisiidowu09@gmail.com>
2022-08-10 11:20:09 +0100

    phase 5 completion of industry page project

--- Zuri_FE_replicate/industry_page/app.js
+++ Zuri_FE_replicate/industry_page/app.js
@@ -3,12 +3,18 @@
 const sections = document.getElementsByClassName('sect7_section')
 
 function handleClick(e) {
-    const secondDiv = e.path[2].querySelectorAll('div')[1];
+    let index = 1
+    console.log(e.path)
+    if (e.path[0].localName != 'div') {
+        index = 2
+    }
+    const secondDiv = e.path[index].querySelectorAll('div')[1];
+    const p = secondDiv.querySelector('p')
+    console.log(p.clientHeight);
     const {height} = secondDiv.style
-    secondDiv.style.height = height == '0px' || !height ? 'auto' : '0px'
+    secondDiv.style.height = height == '0px' || !height ? `${p.clientHeight}px` : '0px'
 }
 
 for (let element of sections) {
-    const span = element.querySelector('span')
-    span.addEventListener('click', handleClick)
+    element.querySelector('.sect7_section1').addEventListener('click', handleClick,true)
 }
